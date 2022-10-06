9d6b8cb6cabe1b886e4a1829434b7a76b0f879c5
alisonorton <orton.alison@gmail.com>
2022-10-06 11:27:40 -1000

    Reading, storing, and displaying data

--- FirstOrder.java
+++ FirstOrder.java
@@ -17,5 +17,11 @@ public class FirstOrder{
 
         System.out.println(s1 + "\n" + c + "\n" + gn + "\n");
 
+        System.out.printf("S(n) = %.1f(n - 1) * %.1f + sigma(%.1f^(n - i) * %.1f)%n", c, s1, c, gn);
+        //System.out.println("S(n) = " + c + "(n - 1) * " + s1 + " sigma(" + c + "^(n - i) * " + gn + ")");
+
+        //Display S(1)
+        System.out.printf("S(1) = %.1f%n", s1);
+
     }
 }
